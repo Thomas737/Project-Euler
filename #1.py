c03c25714609ccabdba784fc75084657ec9273de
Thomas Walsh <60191077+Thomas737@users.noreply.github.com>
2022-11-19 12:46:55 +0000

    final #1 Commit

--- #1.py
+++ #1.py
@@ -1,7 +1,11 @@
+MULTS = [3, 5]
+UP_TO = 1000
+
 def multiples(mults: list, n: int):
-    return sum([divisor for divisor in range(n) if (divisor % mult == 0 for mult in mults)])
+    return clean_up([[divisor for mult in mults if not (divisor % mult)] for divisor in range(n)])
+
+def clean_up(divisors: list):
+    return [divisor[0] for divisor in divisors if len(divisor) > 0]
 
 if __name__ == "__main__":
-    mults = [3, 5]
-    up_to = 1000
-    print(multiples(mults, up_to))
+    print(sum(multiples(MULTS, UP_TO)))
